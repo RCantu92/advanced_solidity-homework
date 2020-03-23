dcedf896eb221ce9858bb6e8c2ee2dab464e561d
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-03-23 16:57:48 -0500

    Updated PupperCoinCrowdsale

--- Code/PupperCoinCrowdsale.sol
+++ Code/PupperCoinCrowdsale.sol
@@ -8,64 +8,117 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 
-// @TODO: Inherit the crowdsale contracts
+/**
+ * @title Pupper Coin Sale
+ * This contract creates a crowdsale for the tokens from the imported
+ * PupperCoin contract. It inherits five contracts from
+ * the OpenZeppelin library.
+ */
 contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
 
+    /**
+    * @dev The parameters are accepted and passed to the
+    * imported OpenZeppelin function to create the crowdsale.
+    * @param rate An integer that is the rate of PupperCoin
+    * to Ether.
+    * @param wallet The main wallet that will have the newly
+    * minted PupperCoin.
+    * @param token The token being used, in this case,
+    * PupperCoin.
+    */
     constructor(
-        // @TODO: Fill in the constructor parameters!
         uint rate,
         address payable wallet,
         PupperCoin token
         )
-        // @TODO: Pass the constructor parameters to the crowdsale contracts.
         
+        /**
+        * @dev The paraments of the constructor are
+        * passed to this inherited function to create
+        * a crowdsale.
+        */
         Crowdsale(
             rate,
             wallet,
             token
             )
         
+        /**
+        * @dev The inherited function is being hardcoded
+        * to cap the crowdsale at 3 Ether, a low amount
+        * for test purposes.
+        */
         CappedCrowdsale(
             3 ether
             )
-            
+        
+        /**
+        * @dev This inherited function is being hardcoded
+        * with the open and close dates. It is set to 1 minute
+        * for test purposes.
+        */
         TimedCrowdsale(
             now,
             now + 1 minutes
             )
-            
+        
+        /**
+        * @dev This inherited function is being hardcoded
+        * the amount of Ether that could be refunded. It is
+        * set to be able to refund the amount it is capped at,
+        * 3 Ether.
+        */
         RefundableCrowdsale(
             3 ether
             )
         
         public
-    {
-        // constructor can stay empty
-    }
+    {}
 }
 
+/**
+ * @title Pupper Coin Sale Deployer
+ * This contract serves to deploy both the PupperCoin
+ * and PupperCoinCrowdsale contracts under a single
+ * transaction.
+ */
 contract PupperCoinSaleDeployer {
 
     address public tokenSaleAddress;
     address public tokenAddress;
 
+    /**
+    * @dev The parameters are accepted and passed to the
+    * PupperCoin and PupperCoinCrowdsale contracts.
+    * @param name a string saved to memory that is
+    * the name of the token.
+    * @param symbol a string saved to memory that is
+    * the symbol (similar to a traditional stock ticker)
+    * of the token.
+    * @param wallet The main wallet that will have the newly
+    * minted PupperCoin.
+    */
     constructor(
-        // @TODO: Fill in the constructor parameters!
         string memory name,
         string memory symbol,
         address payable wallet
     )
         public
     {
-        // @TODO: create the PupperCoin and keep its address handy
+        // Creating the PupperCoin and saving it's address
+        // to the previously declared tokenAddress variable.
         PupperCoin token = new PupperCoin(name, symbol);
         tokenAddress = address(token);
         
-        // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        // Creating the PupperCoinSale and passing it
+        // the rate, wallet, and token parameters. Then
+        // saving its address to the previously declared
+        // tokenSaleAddress variable.
         PupperCoinSale tokenSale = new PupperCoinSale(1, wallet, token);
         tokenSaleAddress = address(tokenSale);
 
-        // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
+        // Making the PupperCoinSale contract a minter,
+        // then have the PupperCoinSaleDeployer renounce its minter role.
         token.addMinter(tokenSaleAddress);
         token.renounceMinter();
     }
